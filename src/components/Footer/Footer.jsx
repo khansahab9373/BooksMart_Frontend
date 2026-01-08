3b472574f37341b398120d65fbc057cade13d8b5
khansahab9373 <abdulrahmankhan1011@gmail.com>
2026-01-08 18:02:05 +0530

    update year

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <div className="bg-gray-100 dark:bg-zinc-800 text-black dark:text-white px-8 py-4">
       <h1 className="text-xl font-semibold text-center">
-        &copy;2024, Made by Abdul Rahman Khan
+        &copy;2026, Made by Abdul Rahman Khan
       </h1>
     </div>
   );
