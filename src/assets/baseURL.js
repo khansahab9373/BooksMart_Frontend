5b4a89ac64333f1524280e6a5515f993365b7bfe
khansahab9373 <abdulrahmankhan1011@gmail.com>
2026-02-14 18:31:01 +0530

    url update

--- src/assets/baseURL.js
+++ src/assets/baseURL.js
@@ -1,6 +1,6 @@
-let BaseULR = `https://booksmart-backend-rose.vercel.app/`;
-
+// let BaseULR = `https://booksmart-backend-rose.vercel.app/`;
 
 // let BaseULR = `http://localhost:3000/`;
+let BaseULR = `https://booksmart-backend-iksy.onrender.com/`;
 
 export default BaseULR;
